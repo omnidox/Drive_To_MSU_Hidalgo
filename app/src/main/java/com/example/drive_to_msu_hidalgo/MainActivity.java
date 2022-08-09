2b26dc241cb4fedb16174cdba69011ca741d8c5c
Rafael Hidalgo <hidalgor2@montclair.edu>
2022-08-09 19:21:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/drive_to_msu_hidalgo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.drive_to_msu_hidalgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Montclair+State+University+Montclair,+New Jersey");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+
+    }
+}
